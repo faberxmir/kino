52fb4808af02d34a907579153f286f5176b0616a
Geir Hilmersen <geir.hilmersen@protonmail.com>
2021-02-17 20:51:40 +0100

    finished working form

--- script/kinobillett.js
+++ script/kinobillett.js
@@ -1,3 +1,30 @@
+let lsOrdersName = "localOrders";
+let orders;
+
+//localStorage.clear();
+
+initPage();
+
+//------------------------------------------------------------------------------------------------------------\\
+function initPage(){
+    orders = localStorage.getItem(lsOrdersName);
+    
+    if(orders==null){
+        orders = [];
+    } else {
+        orders = JSON.parse(orders);
+    }
+}
+
+//This funciton is used for callbacks by the onpage form by id="orderform"
 function buy(){
-    console.log("Kjøp billett!");
+    let insertPosition = orders.length;
+    orders[insertPosition] = {
+        film: document.getElementById("filmmeny").value,
+        navn: document.getElementById("navn").value,
+        telefonnummer: document.getElementById("telefonnummer").value,
+        epost: document.getElementById("email").value
+    }
+    let jsonString = JSON.stringify(orders);
+    localStorage.setItem(lsOrdersName, jsonString);
 }
