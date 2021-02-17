f75d8395f01435361d44db982588a6384b3cf3c7
Geir Hilmersen <geir.hilmersen@protonmail.com>
2021-02-17 22:08:44 +0100

    Added page that lists all orders

--- script/kinobillett.js
+++ script/kinobillett.js
@@ -1,6 +1,5 @@
 let lsOrdersName = "localOrders";
 let orders;
-
 //localStorage.clear();
 
 initPage();
@@ -14,17 +13,68 @@ function initPage(){
     } else {
         orders = JSON.parse(orders);
     }
+    console.log(`The page was initiated!`);
+    console.log(orders);
 }
 
-//This funciton is used for callbacks by the onpage form by id="orderform"
+//This function is used for callbacks by the onpage form by id="orderform"
 function buy(){
-    let insertPosition = orders.length;
-    orders[insertPosition] = {
+    let order = {
         film: document.getElementById("filmmeny").value,
         navn: document.getElementById("navn").value,
+        antall: document.getElementById("antall").value,
         telefonnummer: document.getElementById("telefonnummer").value,
         epost: document.getElementById("email").value
     }
-    let jsonString = JSON.stringify(orders);
-    localStorage.setItem(lsOrdersName, jsonString);
+    let jsonString = JSON.stringify(order);
+
+    //returns false if no orders are equal
+    if(!checkForEqualOrder(jsonString)){
+        let insertPosition = orders.length;
+        orders[insertPosition] = order;
+        localStorage.setItem(lsOrdersName, JSON.stringify(orders));
+        console.log("an order was placed");
+    } else {
+        alert("Duplicate order!");
+    }
+
+}
+
+function getOrdersToHTML(){
+
+    let sectionOrders=document.getElementById("orderList");
+    let htmlString;
+    if(orders.length > 0){
+        console.log(sectionOrders);
+        orders.forEach(order => {
+            sectionOrders.innerHTML += generateTemplate(order.navn, order.film, order.antall, order.telefonnummer, order.epost);
+        });
+    } else {
+        sectionOrders.innerHTML=`<h1> No good movies, no orders! </h1>`
+    }
+}
+
+function generateTemplate(name, film, antall, tlf, epost){
+    const html = `
+        <li class="order">
+            <span class="movietitle">${film}</span>
+            <span class="customer">${name}</span>
+            <span class="ticketNumber">${antall} Billetter</span>
+        </li>
+    `;
+    return html;
+}
+
+function checkForEqualOrder(jsonString){
+    let isEqual = false;
+    if(orders.length > 0){
+        orders.forEach(order => {
+            console.log(jsonString);
+            console.log(JSON.stringify(order));
+            if(JSON.stringify(order) == jsonString){
+                isEqual = true;
+            }
+        });
+    }
+    return isEqual;
 }
